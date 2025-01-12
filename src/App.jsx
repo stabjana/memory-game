5d0116145454e0df6c96ffb724128a094db3dd39
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-01-12 16:53:52 +0200

    move fetching to custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,8 @@
-import axios from "axios";
+import useGetImages from "./hooks/useGetImages"
 import Background from "/src/components/Background/Background.jsx";
 
-const BASE_URL = "https://api.pexels.com/v1/search"; //from Pexels API - used to perform a search query for imgaes based on parameters like keywords
-
-// getting the data
-async function fetchData() {
-  try {
-    const response = await axios.get(BASE_URL, {
-      headers: {
-        Authorization: import.meta.env.VITE_AUTH_KEY, // API-key from .env-Datei
-      },
-    });
-    console.log(response.data);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-}
-
 function App() {
-  fetchData();
+  const images = useGetImages();
 
   return (
     <>
--- src/hooks/useGetImages.js
+++ src/hooks/useGetImages.js
@@ -1,5 +1,52 @@
-const BASE_URL = 'https://api.pexels.com/v1/search';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const useGetImages = () => {};
+// const BASE_URL = 'https://api.pexels.com/v1/search';
+
+const getRandomPage = () => Math.round(Math.random() * (10 - 1) + 1);
+
+const useGetImages = () => {
+    const [images, setImages] = useState([]);
+
+    const buildUrl = () => {
+        const url = new URL("https://api.pexels.com/v1/search");
+        url.search = new URLSearchParams({
+            query: "nature", // TO DO: Replace with a variable for dynamic queries
+            orientation: "square",
+            size: "small",
+            per_page: 2, // TO DO: Replace with a variable
+            page: getRandomPage()
+        });
+        return url.toString(); // Convert the URL object to a string
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // Use the buildUrl function to generate the URL
+                const url = buildUrl();
+
+                const response = await axios.get(url, {
+                    headers: {
+                        Authorization: import.meta.env.VITE_AUTH_KEY,
+                    },
+                });
+                console.log("Pexels API response data:", response.data);
+                setImages(data.photos);
+
+            } catch (error) {
+                if (error.response) {
+                    console.error("Error response:", error.response);
+                } else {
+                    console.error("Error message:", error.message);
+                }
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return images;
+};
 
 export default useGetImages;
