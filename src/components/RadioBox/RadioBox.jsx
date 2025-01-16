312b6994e9c94966eb0f150beaa5ab9b15a670b3
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-01-16 20:35:43 +0200

    add and refactor Settings component

--- src/components/RadioBox/RadioBox.jsx
+++ src/components/RadioBox/RadioBox.jsx
@@ -3,8 +3,12 @@ import PropTypes from 'prop-types';
 
 import styles from './RadioBox.module.css';
 
-const RadioBox = () => {
-    return <p>RadioBox</p>;
+const RadioBox = ({ name }) => {
+    return (
+        <div>
+            <input type="radio" name={name} id={name} />
+        </div>
+    );
 };
 
 export default RadioBox;
--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { CATEGORIES } from '../../constants.js'
 import PropTypes from 'prop-types';
 
 import styles from './Settings.module.css';
 
 const Settings = () => {
-    return <p>Settings</p>;
+    return (
+
+        <div className={styles.settings}>
+            <h2>Settings</h2>
+            <h4>Category:</h4>
+            <div className={styles.settings}>
+                {CATEGORIES.map(item => (
+                    <RadioBox key={item} name={item} />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Settings;
